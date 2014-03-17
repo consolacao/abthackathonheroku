8018378132a8fbb4d27ce3837f3768bac25efe5e
Harald Haesler <github@werkkreis.org>
2014-03-17 16:00:55 +0100

    Initial Commit

---
+++ controllers/home.js
@@ -0,0 +1,16 @@
+/**
+ * GET /
+ * Home page.
+ */
+
+exports.index = function(req, res) {
+  res.render('home', {
+    title: 'Home'
+  });
+};
+
+exports.escapeVelocity = function(req, res) {
+  res.render('escape_velocity', {
+    title: 'Landing Page'
+  });
+};
